036aaa9c09b41430b94f53b7718d4accb19cdb2b
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-12 09:45:15 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,10 @@
 def my_select(collection)
  i = 0 
+ new_collection = []
  
  while i < collection.length 
   new_collection << yield(collection[i].include?())
   i += 1  
  end
+ new_collection
 end
